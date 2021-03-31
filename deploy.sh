1095ca7d324b5a85e8194c31f52e3a321f00eedd
Pupper0601 <gdmuye@qq.com>
2021-03-31 19:12:11 +0800

    coding推送调整

--- deploy.sh
+++ deploy.sh
@@ -32,7 +32,7 @@ git push -f $githubUrl main:gh-pages # 推送到github
 
 
 # # deploy to coding
-# echo 'www.pupper.cn\pupper.cn' > CNAME  # 自定义域名
+# echo 'pupper.cn' > CNAME  # 自定义域名
 # echo 'google.com, pub-7828333725993554, DIRECT, f08c47fec0942fa0' > ads.txt # 谷歌广告相关文件
 
 # if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
